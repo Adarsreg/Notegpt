f1e74c1d018761122b5cb23607217c4d17e4faa6
Adarsh <adarshregmi1@gmail.com>
2023-03-31 10:25:12 +0530

    connected mongo

--- notegpt/Server/Server.js
+++ notegpt/Server/Server.js
@@ -1,4 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('mongoose')
 
-require('dotenv').config()
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const dburl = process.env.MONGO_URL;
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err))
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
