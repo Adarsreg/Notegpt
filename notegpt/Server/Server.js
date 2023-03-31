caa4d01999adb3a5c79ace87f3126f03c683c225
Adarsh <adarshregmi1@gmail.com>
2023-03-31 11:19:33 +0530

    rm index.js

--- notegpt/Server/Server.js
+++ notegpt/Server/Server.js
@@ -6,7 +6,7 @@ const routes = require('./routes/noteroute')
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 const dburl = process.env.MONGO_URL;
 
 mongoose.connect(process.env.MONGODB_URL)
