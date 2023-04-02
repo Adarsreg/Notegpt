ef239e414ce1bb0a486a35af6ae65d51c29a41a4
Adarsh <adarshregmi1@gmail.com>
2023-04-03 00:50:08 +0530

    test1

--- notegpt/Server/routes/noteroute.js
+++ notegpt/Server/routes/noteroute.js
@@ -3,13 +3,15 @@ const Note = require('../models/notemodel');
 
 const router = express.Router();
 const noteController = require("../controllers/notecontrol")
-router.get('/', noteController.getAllNotes);
+router.get('/', (req, res) => {
+    res.json({ message: "Hello world" })
+})
 
 // GET a single note by ID
-router.get('/:id', noteController.getNoteById);
+//router.get('/:id', noteController.getNoteById);
 
 // POST a new note
-router.post('/api/notes', noteController.createNote);
+router.post('/save', noteController.createNote);
 
 // DELETE a note by ID
 router.delete('/:id', noteController.deleteNoteById);
