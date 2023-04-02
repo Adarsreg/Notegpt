53f849a94a0e3dd0b99e3358f42e3487a6cf8b5a
Adarsh <adarshregmi1@gmail.com>
2023-04-02 22:50:53 +0530

    init  endpoints

--- notegpt/Server/routes/noteroute.js
+++ notegpt/Server/routes/noteroute.js
@@ -1,7 +1,17 @@
-const { Router } = require('express')
+const express = require('express');
+const Note = require('../models/notemodel');
 
-const router = Router()
+const router = express.Router();
+const noteController = require("../controllers/notecontrol")
+router.get('/', noteController.getAllNotes);
 
-router.get('/', (req, res) => { res.json({ message: 'Welcome to my API' }) })
+// GET a single note by ID
+router.get('/:id', noteController.getNoteById);
+
+// POST a new note
+router.post('/api/notes', noteController.createNote);
+
+// DELETE a note by ID
+router.delete('/:id', noteController.deleteNoteById);
 
 module.exports = router;
